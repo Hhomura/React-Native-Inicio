6e97e58a0563a5c93abb2282d52a771df0e9e427
Felipe Santos <felipesantosramos2004@gmail.com>
2023-02-05 16:32:29 -0400

    Inicio das Telas

---
+++ Teste/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default (() =>{
+  return(
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  )
+})
---
+++ Teste/Telas/Forms.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+export default class Forms extends Component {
+    render(){
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
